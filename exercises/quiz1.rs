136bdff5b5103564c7301a7731a35d784a2ef8c0
Michael Fatjanov <michael.fatjanov@axians.com>
2024-02-12 16:34:35 +0100

    quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,8 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
@@ -30,4 +28,12 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(amountOfApples: i32) -> i32 {
+    if amountOfApples > 40 {
+        return amountOfApples 
+    } else {
+        return amountOfApples * 2
+    }
 }
